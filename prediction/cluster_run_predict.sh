c04081d054b9b6733acab4fe20cc9e92787e7661
xuhaoshu <haoshuxu.smile@gmail.com>
2025-12-02 21:40:45 +0800

    cluster submit

--- prediction/cluster_run_predict.sh
+++ prediction/cluster_run_predict.sh
@@ -2,13 +2,13 @@
 #SBATCH --job-name=submit
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=8
 #SBATCH -p q_fat_c
 #SBATCH -q high_c
-#SBATCH -o /ibmgpfs/cuizaixu_lab/xuhaoshu/code/WM_prediction/log/predict/ABCD/cognition/job.%j.out
-#SBATCH -e /ibmgpfs/cuizaixu_lab/xuhaoshu/code/WM_prediction/log/predict/ABCD/cognition/job.%j.error.txt
+#SBATCH -o /ibmgpfs/cuizaixu_lab/xuhaoshu/code/WM_prediction/log/predict/ABCD/pfactor/job.%j.out
+#SBATCH -e /ibmgpfs/cuizaixu_lab/xuhaoshu/code/WM_prediction/log/predict/ABCD/pfactor/job.%j.error.txt
 
 source /GPFS/cuizaixu_lab_permanent/xuhaoshu/miniconda3/bin/activate
 conda activate ML
 
-python /ibmgpfs/cuizaixu_lab/xuhaoshu/code/WM_prediction/src/prediction/predict_age_RandomCV.py
+python /ibmgpfs/cuizaixu_lab/xuhaoshu/code/WM_prediction/src/prediction/predict_pfactor_RandomCV.py
